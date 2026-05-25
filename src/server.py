7ea71288bd9e52dc7c7e1a28802eaa70f3f39074
Piotr Foltyniewicz <piotr.foltyniewicz@gmail.com>
2026-05-25 22:58:44 +0200

    cleaner static storage instatiation

--- src/server.py
+++ src/server.py
@@ -5,12 +5,11 @@ from fastmcp import FastMCP
 from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
 
-from services.static_storage import ReadOnlyStaticStorage, StaticStorage
+from services.static_storage import ReadOnlyStaticStorage, StaticStorage, WriteOnlyStaticStorage
 from services.ztm_service import ZTMService
 
 mcp = FastMCP("ztm-poznan")
 ztm_service = ZTMService.instance()
-static_storage: ReadOnlyStaticStorage = StaticStorage.instance()
 
 @mcp.tool()
 def echo(text: str) -> str:
@@ -21,6 +20,7 @@ def echo(text: str) -> str:
 @mcp.tool()
 def list_routes_and_stops() -> dict[str, list[dict[str, str]]]:
     """List all routes and stops from static GTFS data."""
+    static_storage: ReadOnlyStaticStorage = StaticStorage.instance()
     with static_storage.read_lock():
         routes = static_storage.get_routes()
         stops = static_storage.get_stops()
@@ -31,6 +31,7 @@ def list_routes_and_stops() -> dict[str, list[dict[str, str]]]:
 
 
 if __name__ == "__main__":
+    static_storage: WriteOnlyStaticStorage = StaticStorage.instance()
     ztm_service.start_daily_refresh(static_storage)
     mcp.run(
         transport="sse",
